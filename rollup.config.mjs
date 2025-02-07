cdf571cf80f7ff2a7433b89fb6d2144cc2748f82
darcy <darcy_joven@live.com>
2025-02-07 11:28:12 +0800

    🐞 fix(sourceMap): 修复sourceMap格式错误

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,6 +5,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import screeps from 'rollup-plugin-screeps';
+import { readFileSync,writeFileSync } from 'fs';
 
 let cfg;
 const dest = process.env.DEST;
@@ -12,6 +13,19 @@ if (!dest) {
   console.log("No destination specified - code will be compiled but not uploaded");
 } else if ((cfg = require("./screeps.json")[dest]) == null) {
   throw new Error("Invalid upload destination");
+}
+/**@type {() => import("rollup").Plugin} */
+function sourcemapToJs() {
+  return {
+    writeBundle(options) {
+      if (options.sourcemap && options.file) {
+        const str = readFileSync(`${options.file}.map`, {
+          encoding: "utf-8",
+        });
+        writeFileSync(`${options.file}.map`, `module.exports = ${str}`);
+      }
+    },
+  };
 }
 
 export default {
@@ -27,6 +41,7 @@ export default {
     resolve({ rootDir: "src" }),
     commonjs(),
     typescript({tsconfig: "./tsconfig.json"}),
+    sourcemapToJs(),
     screeps({config: cfg, dryRun: cfg == null})
   ]
 }
