1de3cd9d8f0bb12e6cf1259ec0031546510886d6
darcy <darcy_joven@live.com>
2025-02-12 17:16:35 +0800

    ✨ feat(room): 添加房间配置扩展
    - 新增 ConfigExtension 类用于管理房间配置
    - 实现 addAvoidPos 方法用于添加禁止通过的位置
    - 将 ConfigExtension 加入房间插件列表

---
+++ src/mount/room/config.ts
@@ -0,0 +1,11 @@
+/**
+ * 一些房间内的配置
+ */
+export default class ConfigExtension extends Room {
+    /**
+     * 添加不允许通过position
+     */
+    public addAvoidPos(creepName: string, pos: RoomPosition):void {
+
+    }
+}
--- src/mount/room/index.ts
+++ src/mount/room/index.ts
@@ -1,9 +1,10 @@
 import LayoutExtension from "./layout";
 import SearchExtension from "./search";
 import TaskExtension from "./task";
+import ConfigExtension from "./config";
 import { assignPrototype } from "utils/mount";
 
-const plugins = [LayoutExtension, SearchExtension, TaskExtension]
+const plugins = [ConfigExtension, LayoutExtension, SearchExtension, TaskExtension]
 
 export default () => plugins.forEach(plugin => { assignPrototype(Room, plugin) })
 
